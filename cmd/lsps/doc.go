53bda13be66d9f158469237a3f985e7a95344444
Adam Chalkley <atc0005@users.noreply.github.com>
2022-12-06 03:28:35 -0600

    Fix overview statements
    - README overview details
    - lsps overview doc comments

--- cmd/lsps/doc.go
+++ cmd/lsps/doc.go
@@ -5,7 +5,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for
 // full license information.
 
-// Small CLI tool to list processes with known problematic processes.
+// Small CLI tool to list processes with known problematic states.
 //
 // See our [GitHub repo]:
 //
